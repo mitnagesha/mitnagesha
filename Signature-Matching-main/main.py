9dac826f787cd6fc11aaf9e4e4b105878e0e2d30
nagesha <nageshauppara2003@gmail.com>
2024-11-25 15:25:26 +0530

    change the png

--- Signature-Matching-main/main.py
+++ Signature-Matching-main/main.py
@@ -15,7 +15,7 @@ logging.basicConfig(level=logging.DEBUG)
 
 def browsefunc(ent):
     filename = askopenfilename(filetypes=[("image", ".jpeg"),
-                                          ("image", ".png"),
+                                          ("image", ".kkk"),
                                           ("image", ".jpg"),])
     ent.delete(0, tk.END)
     ent.insert(tk.END, filename)
